13c38bcee1cc32b1cc806932c6fa2a2536b9a823
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-17 13:49:02 +0530

    variable declaration

---
+++ HTMLProgs/Self Works/JavaScript/4.js
@@ -0,0 +1,23 @@
+var b=64;
+var b=89;
+var b=56;
+console.log("b="+b);
+
+
+let a=64;
+//if you write let a=84; then it will be error because let can only be updated not declared
+a=84;
+console.log("a="+a);
+
+
+const c=54;
+//const give a fixed number which cannot be updated or declared later...
+console.log("c="+c);
+
+let p;
+console.log(p)
+//for using let if we don't give the value to variable then it gives undefined.
+
+var o;
+console.log(o)
+
