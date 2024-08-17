df71feef39aed8923715944a8cae44a2e7f82745
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-18 02:35:08 +0530

    Practice Question 2

---
+++ HTMLProgs/Self Works/JavaScript/prac2.js
@@ -0,0 +1,10 @@
+const profile={
+    posts:195,
+    followers:"569k",
+    following:4,
+    name:"Shardha khapra",
+    threads:"shradhakapra",
+    job:"Enterprenueur",
+
+};
+console.log(profile);
