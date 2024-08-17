5ac4ba4986a4a17f14dd08642e22c5a4624c140a
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-18 02:24:54 +0530

    Practice Question 1

---
+++ HTMLProgs/Self Works/JavaScript/prac.js
@@ -0,0 +1,10 @@
+const product={
+    productName:"Parker Jotter Standard CT Ball Pen(Black)",
+    ratings:4,
+    reviews:7002,
+    MRP:285,
+    discount:"5%",
+    price:270,
+};
+
+console.log(product);
