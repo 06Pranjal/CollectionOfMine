786e260fe0664ffd47f6f70e7d2cb130e2e831da
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-08-17 12:35:01 +0530

    variable declaration

--- HTMLProgs/Self Works/JavaScript/3.js
+++ HTMLProgs/Self Works/JavaScript/3.js
@@ -1,1 +1,6 @@
- 
+ a=64;
+ console.log(a);
+ a="Pranjal Singh";
+ console.log(a);
+ a=5.0;
+ console.log(a);
--- HTMLProgs/Self Works/JavaScript/Second.js
+++ HTMLProgs/Self Works/JavaScript/Second.js
@@ -1,5 +1,11 @@
 console.log("this is code to demonstrate the variables in javascript")
-let a=67
-console.log(a)
-a="Pranjal"
-console.log(a)
+a=67;
+console.log(a);
+a="Pranjal";
+console.log(a);
+x=null;
+console.log(x);
+y=false;
+console.log(y);
+z=undefined;
+console.log(z);
