e9532dd7af329ad67689aa09c489cc644f1de67b
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-09-01 03:16:22 +0530

    Modification

--- HTMLProgs/Self Works/JavaScript/9.js
+++ HTMLProgs/Self Works/JavaScript/9.js
@@ -1,2 +1,15 @@
-const a=5;
-console.log(a);
+//conditional statement
+//if satatement 
+let age=28;
+if(age>18){
+    console.log("Able");
+} 
+
+//if else
+let fullName="Pranjal";
+if(fullName=="Singh"){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
