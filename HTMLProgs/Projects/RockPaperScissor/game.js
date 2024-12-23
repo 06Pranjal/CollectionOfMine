4ad066a93975f542db1b26e1199a6dfa0a4e5449
Pranjal Singh <pranjalsingh0605@gmail.com>
2024-12-23 18:58:53 +0530

    adding score objects in this game

--- HTMLProgs/Projects/RockPaperScissor/game.js
+++ HTMLProgs/Projects/RockPaperScissor/game.js
@@ -1,5 +1,9 @@
 console.log("Welcome to classic Rock Paper Scissors")
-
+const score={
+    win:0,
+    loose:0,
+    ties:0
+}
 function computerMove(){
 
 const numberComputer= Math.random();
@@ -25,6 +29,11 @@ function win(player){
     let result;
     if (player===moveComputer){
         result= "Match Tie";
+        console.log(result);
+        score.ties++;
+        console.log(score);
+        alert(`Match Tie!\nWins: ${score.win}, Losses: ${score.loose}, Ties: ${score.ties}`);
+
     }
 
     else if((player==='rock'&& moveComputer==='scissors') || 
@@ -32,11 +41,32 @@ function win(player){
     (player==='scissors'&& moveComputer==='paper')
 ){
         result= "Player Wins";
+        
+        console.log(result);
+        score.win++;
+        console.log(score);
+        alert(`Player wins!\nWins: ${score.win}, Losses: ${score.loose}, Ties: ${score.ties}`);
+
     }
     else {
         result= "Computer Wins";
+        console.log(result);
+        score.loose++;
+        console.log(score);
+        alert(`Computer wins!\nWins: ${score.win}, Losses: ${score.loose}, Ties: ${score.ties}`);
+
     }
     
-    console.log(result);
+    
+
+}
+
+function resetscore(){
+    score.win=0;
+    score.loose=0;
+    score.ties=0;
+    console.log("Score Reset");
+    console.log(score);
+    alert(`Score Reset!\nWins: ${score.win}, Losses: ${score.loose}, Ties: ${score.ties}`);
 
 }
