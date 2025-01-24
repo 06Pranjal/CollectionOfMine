ed3b20f9e1649d02df71ff975a2e6980591f082b
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-01-24 15:35:17 +0530

    the file got something missed but done it later

--- HTMLProgs/Self Works/React/index.jsx
+++
@@ -1,9 +0,0 @@
-import {createroot} from "react-dom/client"
-
-
-const root=createroot(document.getElementById("root"))
-
-root.render(<h1>Hello React</h1>)
-
-//we can also write 
-//createroot(document.getElementById("root")).render(<h1>Hello React</h1)
--- README.md
+++
@@ -1,9 +0,0 @@
-# CollectionOfMine
-PRANJAL SINGH
-<br>
-This is a Tutorial to GitHub.
-<br>
-This is my first Repositories.
-<br>
-Hey there
-....
